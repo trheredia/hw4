ae0bc294d1461b60dfe23015badcc75ecaf2f26f
Theodora Heredia <theredia@usc.edu>
2025-03-31 03:06:23 +0000

    finished equal-paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -6,13 +6,63 @@
 #include "equal-paths.h"
 using namespace std;
 
-
 // You may add any prototypes of helper functions here
+bool pathHelper(Node* node, int depth, int& leaf){
+  
+  if(node == nullptr){ // base case
+    return true;
+  }
+
+  if(node->left == nullptr && node->right == nullptr){ // leaf node
+    if(leaf == -1){ // remember the depth if it's the first leaf
+      leaf = depth;
+    }
+    if(depth == leaf){ // compare current leaf and first leaf 
+      return true;
+    } 
+    else{
+      return false;
+    }
+  }
+
+  bool leftRecursion; 
+  leftRecursion = pathHelper(node->left, depth + 1, leaf); // recurse throught left subtree
+  bool rightRecursion;
+  rightRecursion = pathHelper(node->right, depth + 1, leaf); // recurse through right subtree
+
+  if(leftRecursion && rightRecursion){ // return if both are true
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    if(root == nullptr){ // base case
+      return true;
+    }
+
+    Node* currentNode = root; 
+    int depth = 1;
+
+    if(currentNode->left){
+      depth++;
+      currentNode = currentNode->left;
+      equalPaths(currentNode);
+    }
+
+    if(currentNode->right){
+      depth++;
+      currentNode = currentNode->right;
+      equalPaths(currentNode);
+    }
+
+    int leaf = -1; // no leaf yet
+    return pathHelper(root, 0, leaf);
 
 }
 
